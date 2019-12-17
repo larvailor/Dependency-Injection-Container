4cff6da401cd69b60a3866b42537d4972d30ff69
Dima Konovko <dima.konovko@mail.ru>
2019-12-17 20:02:09 +0300

    development; add: logic for resolving base case

--- Dependency-Injector/Dependency-Injector/DependencyInjector.cs
+++ Dependency-Injector/Dependency-Injector/DependencyInjector.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,18 +22,93 @@ namespace Dependency_Injector
 
 
 
-        public object Resolve<TDependency>()
+        public TDependency Resolve<TDependency>()
         {
-            Type tDependency = typeof(TDependency);
+            return (TDependency)Resolve(typeof(TDependency));
+        }
 
+
+
+        private object Resolve(Type tDependency)
+        {
             object result = null;
+
             if (!DiConfig.dDepImpl.Any())
             {
                 return result;
             }
 
+            if (DiConfig.dDepImpl.ContainsKey(tDependency))
+            {
+                var implementations = new List<Implementation>(DiConfig.GetImplementationsForDependency(tDependency));
+                if (implementations.Any())
+                {
+                    result = HandleSingletonCase(implementations[0]);
+                }
+            }
+            else
+            {
+                result = CreateUsingConstructor(tDependency);
+            }
+   
+            return result;
+        }
 
 
+
+        private object HandleSingletonCase(Implementation implementation)
+        {
+            object result = null;
+            if (implementation.IsSingleton)
+            {
+                if (implementation.Singleton != null)
+                {
+                    result = implementation.Singleton;
+                }
+                else
+                {
+                    result = Resolve(implementation.Type);
+                    implementation.Singleton = result;
+                }
+            }
+            else
+            {
+                result = Resolve(implementation.Type);
+            }
+            
+            return result;
+        }
+
+
+
+        private object CreateUsingConstructor(Type type)
+        {
+            object result = null;
+
+            var constructorsInfo = type.GetConstructors();
+            foreach (var constructorInfo in constructorsInfo)
+            {
+                var parameters = new List<object>();
+
+                try
+                {
+                    var paramsInfo = constructorInfo.GetParameters();
+                    foreach (var paramInfo in paramsInfo)
+                    {
+                        parameters.Add(Resolve(paramInfo.ParameterType));
+                    }
+
+                    result = Activator.CreateInstance(type, parameters.ToArray());
+                    if (result != null)
+                    {
+                        return result;
+                    }
+                }
+                catch
+                {
+                    // do nothing 
+                }
+            }
 
             return result;
         }
