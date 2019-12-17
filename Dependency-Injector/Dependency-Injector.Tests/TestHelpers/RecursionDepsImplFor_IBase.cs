6dfe560ab18c219356962bc983ce982c6c6425e6
Dima Konovko <dima.konovko@mail.ru>
2019-12-18 01:40:12 +0300

    development; add: test for handling infinite recursion
    add: RecursionDepsImplFor_IBase	helper class with recursion problem

---
+++ Dependency-Injector/Dependency-Injector.Tests/TestHelpers/RecursionDepsImplFor_IBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector.Tests.TestHelpers
+{
+    public class RecursionDepsImplFor_IBase : IBase
+    {
+        // variables
+        public IBase RecursiveField { get; private set; }
+
+
+
+        // methods
+        public RecursionDepsImplFor_IBase(IBase iBase)
+        {
+            RecursiveField = iBase;
+        }
+
+
+
+        public void BaseMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
