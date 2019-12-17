c45d2ff73f22ad1418be9030fe232b312f13c082
Dima Konovko <dima.konovko@mail.ru>
2019-12-18 02:19:25 +0300

    development; add: test for multiple impls for one dep

---
+++ Dependency-Injector/Dependency-Injector.Tests/TestHelpers/ImplFor_IBase_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector.Tests.TestHelpers
+{
+    public class ImplFor_IBase_2 : IBase
+    {
+        public void BaseMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
