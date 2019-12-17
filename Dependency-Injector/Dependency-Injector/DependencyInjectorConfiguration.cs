27a070c805f4a879c635460b051f6ae6aabbde1a
Dima Konovko <dima.konovko@mail.ru>
2019-12-17 12:53:32 +0300

    development; add: register logic
    upd: DiC to register dependencies and implementations correctly
    add: Implementation class skeleton for handling info for implementation

--- Dependency-Injector/Dependency-Injector/DependencyInjectorConfiguration.cs
+++ Dependency-Injector/Dependency-Injector/DependencyInjectorConfiguration.cs
@@ -9,20 +9,40 @@ namespace Dependency_Injector
     public class DependencyInjectorConfiguration
     {
         // variables
+        public Dictionary<Type, List<Implementation>> dDepImpl { get; private set; } 
 
 
 
         // methods
         public DependencyInjectorConfiguration()
         {
-
+            dDepImpl = new Dictionary<Type, List<Implementation>>();
         }
 
 
 
         public void Register<TDependency, TImplementation>()
         {
-            throw new NotImplementedException();
+            Type tDependency = typeof(TDependency);
+            Type tImplementation = typeof(TImplementation);
+
+            if (tImplementation.IsAbstract)
+            {
+                throw new ArgumentException("Register failed. Implementation could not be abstract");
+            }
+
+            if (!tDependency.IsAssignableFrom(tImplementation))
+            {
+                throw new ArgumentException("Register failed. Dependency is not assignable from implementation");
+            }
+
+            List<Implementation> implsForSpecificDependency;
+            if (!dDepImpl.TryGetValue(tDependency, out implsForSpecificDependency))
+            {
+                implsForSpecificDependency = new List<Implementation>();
+                dDepImpl[tDependency] = implsForSpecificDependency;
+            }
+            implsForSpecificDependency.Add(new Implementation(tImplementation));
         }
     }
 }
---
+++ Dependency-Injector/Dependency-Injector/Implementation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector
+{
+    public class Implementation
+    {
+        // variables
+        public Type Type { get; private set; }
+
+
+        // methods
+        public Implementation(Type tImplementation)
+        {
+            Type = tImplementation;
+        }
+    }
+}
