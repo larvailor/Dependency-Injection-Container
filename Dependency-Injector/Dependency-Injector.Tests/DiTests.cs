de52c8dedcd784fdba33a1dc76e5b12bc1a4a2d7
Dima Konovko <dima.konovko@mail.ru>
2019-12-17 12:50:10 +0300

    development; add: DIConfig_ShouldRegister_IfCorrect test, add: helpers"
    mv: UnitTests1.cs to DiTests.cs
    add: TestHelpers
    	IBase as base interface
    	ImplFor_IBase as implementation for IBase

---
+++ Dependency-Injector/Dependency-Injector.Tests/DiTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Dependency_Injector.Tests.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dependency_Injector.Tests
+{
+    [TestClass]
+    public class DiTests
+    {
+        [TestMethod]
+        public void DIConfig_ShouldRegister_IfCorrect()
+        {
+            var diConfig = new DependencyInjectorConfiguration();
+            diConfig.Register<IBase, ImplFor_IBase>();
+
+            var keys = new List<Type>(diConfig.dDepImpl.Keys);
+            var values = new List<List<Implementation>>(diConfig.dDepImpl.Values);
+
+            Assert.AreEqual(1, keys.Count);
+            Assert.AreEqual("IBase", keys[0].Name);
+            Assert.AreEqual(1, values.Count);
+            Assert.AreEqual(1, values[0].Count);
+            Assert.AreEqual("ImplFor_IBase", values[0][0].Type.Name);
+        }
+    }
+}
