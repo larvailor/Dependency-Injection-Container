e09cff7e9cce0dbebf424946690a1b556f8dfdfe
Dima Konovko <dima.konovko@mail.ru>
2019-12-17 10:50:21 +0300

    development; add: DI and DIConfig classes with empty methods

---
+++ Dependency-Injector/Dependency-Injector/DependencyInjector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector
+{
+    public class DependencyInjector
+    {
+        // variables
+
+
+
+        // methods
+        public DependencyInjector(DependencyInjectorConfiguration diConfig)
+        {
+
+        }
+
+
+
+        public object Resolve<TDependency>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dependency-Injector/Dependency-Injector/DependencyInjectorConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector
+{
+    public class DependencyInjectorConfiguration
+    {
+        // variables
+
+
+
+        // methods
+        public DependencyInjectorConfiguration()
+        {
+
+        }
+
+
+
+        public void Register<TDependency, TImplementation>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
