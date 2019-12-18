03d07a471a20cd4d05aec2e968f3a1cfc799946c
Dima Konovko <dima.konovko@mail.ru>
2019-12-18 10:45:31 +0300

    development; add: test for generic dependency
    add: IBaseWithDependency helper interface that has inner dependencies
    add: ImplFor_IBaseWithDependency  helper class that implements
    IBaseWithDependency interface

---
+++ Dependency-Injector/Dependency-Injector.Tests/TestHelpers/ImplFor_IBaseWithDependency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injector.Tests.TestHelpers
+{
+    public class ImplFor_IBaseWithDependency<ImplFor_IBase> : IBaseWithDependency<ImplFor_IBase> where ImplFor_IBase : IBase
+    {
+        public void BaseMethod2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
